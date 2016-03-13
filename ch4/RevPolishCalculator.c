83ad9cfcd95c1b79a32a6431f2631aa7634a97dc
qiongsiwu <qiongsiwu@gmail.com>
2016-03-13 11:16:56 -0400

    Exercise 4-3

--- ch4/RevPolishCalculator.c
+++ ch4/RevPolishCalculator.c
@@ -1,4 +1,5 @@
 /* Example in Section 4.3
+   Exercise 4-3
  */
 
 #include <stdio.h>
@@ -11,8 +12,10 @@ int getop(char []);
 void push(double); 
 double pop(void); 
 void printstack(); 
+void printbuf(); 
 
 /* reverse Polish calculator */ 
+/* Exercise 4-3: added % operator */
 int main () 
 {
     int type; 
@@ -41,12 +44,20 @@ int main ()
 	    else
 		printf("error:zero divisor\n"); 
 	    break; 
+	case '%':
+	    op2 = pop(); 
+	    if (op2 != 0.0)
+		push((int)pop() % (int)op2); 
+	    else
+		printf("error:zero divisor in modulus calculation\n"); 
+	    break; 
 	case '\n':
 	    printf("\t%.8g\n", pop()); 
 	    break; 
 	default:
 	    printf("error: unknown command %s\n", s); 
 	}
+	//printstack(); 
     }
     return 0; 
 }
@@ -93,6 +104,7 @@ double pop(void)
 int getch(void); 
 void ungetch(int); 
 
+/* Exercise 4-3, added provision for negative numbers */
 int getop(char s[])
 {
     int i, c; 
@@ -100,16 +112,26 @@ int getop(char s[])
     while ((s[0] = c = getch()) == ' ' || c == '\t')
 	; 
     s[1] = '\0'; 
-    if (!isdigit(c) && c != '.')
+    if (!isdigit(c) && c != '.' && c != '-')
 	return c; /* not a number */
+    
+    if (c == '-') {
+	if (!isdigit(c = getch())) {  /* if the '-' is minus */
+	    ungetch(c);  
+	    return '-'; 
+	} else                        /* if the '-' is the negative sign */
+	    ungetch(c);
+    }
+
     i = 0; 
     if (isdigit(c)) /* collect integer part */
 	while (isdigit(s[++i] = c = getch()))
 	    ;
     if (c == '.')
-	while (isdigit(s[++i] = c = getch()))
+	while (isdigit(s[++i] = c = getch())) /* collect the fraction part */
 	    ; 
     s[i] = '\0'; 
+
     if (c != EOF)
 	ungetch(c); 
     return NUMBER; 
@@ -131,4 +153,13 @@ void ungetch(int c) /* push character back on input */
 	printf("ungetch: too many characters\n"); 
     else
 	buf[bufp++] = c; 
+}
+
+/* function to debug the buf */
+void printbuf()
+{
+    printf("Current buf:\t"); 
+    for (int i = 0; i < bufp; i++)
+	printf("%d\t", buf[i]); 
+    printf("\n"); 
 }
